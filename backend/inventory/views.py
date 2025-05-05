a97add93a305aa8a62bd16e34c5d06053ca3eed8
dmootb <srmootdev@gmai.com>
2025-05-04 20:34:03 -0500

    Update frontend

--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
 # Create your views here.
+
+class InventoryViewSet(ModelViewSet):
+    
+    def list() -> None:
+        pass
+    
